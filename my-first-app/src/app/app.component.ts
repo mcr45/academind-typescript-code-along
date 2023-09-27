06a2754c4914b84e95984b79b88efce754642e21
mcr45 <depr3604@protonmail.com>
2023-09-26 23:48:18 -0500

    angular components manually and with cli

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']// I can change style from here
 })
 export class AppComponent {
   name= 'Cesar';

da661825063000ee70a6cd974ac175a0c3736960
mcr45 <depr3604@protonmail.com>
2023-09-27 18:34:26 -0500

    applied string interpotation. Data Binding is cool

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -5,4 +5,11 @@ selector:"app-server",
 templateUrl:"./server.component.html"
 }
 )
-export class ServerComponent{}
+export class ServerComponent{
+  serverId:number=117
+  serverStatus:string='Cloud'
+
+  getDate(){
+    return new Date()
+  }
+}

52fe1b5a14a56c86fe764014cfd182a2f798d88f
mcr45 <depr3604@protonmail.com>
2023-09-29 20:11:12 -0500

    implemented ngstyle ngif ngfor directives. kinda cool

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -5,6 +5,9 @@ selector:"app-server",
 templateUrl:"./server.component.html",
 styles: [`
 .online{color:pink}
+.sowhite{
+  color:red
+}
 `]
 
 }
@@ -12,7 +15,9 @@ styles: [`
 export class ServerComponent{
   serverId:number=117
   serverStatus:string='Cloud'
-
+  showtime:boolean=true
+  scusisadirmilora:string[]=[]
+  bgcolor:string=''
   getServerStatus(){
     this.serverStatus= Math.random() > 0.5 ? "ofline" : "online"
   }
@@ -20,8 +25,17 @@ export class ServerComponent{
   getColor(){
     return this.serverStatus==="ofline" ? 'blue' : "green"
   }
-
+  onTime(){
+    this.showtime=this.showtime? false:true
+    this.scusisadirmilora.push("button has been clicked, I repeat button has been clicked")
+  }
   getDate(){
     return new Date()
+  }
+  getBlue(){
+    if(this.scusisadirmilora.length>4){
+      this.bgcolor="blue"
+      return 'true'
+    }
   }
 }
